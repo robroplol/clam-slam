f17e1471b9bdfdde2dd08d065ab9cdbc3cbe0a24
robroplol <robrop92@gmail.com>
2022-02-23 16:21:50 -0500

    fixed grid, added title

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,11 +1,11 @@
-<div class="p-12 m-16 h-48">
-    <h2>{{$name}}</h2>
-    <div class="grid grid-cols-2">
-        <div class="h-56 flex flex-col content-end">
+<div class="p-4 m-4 border-purple-600	border-2">
+    <h2 class="text-xl text-center">{{$name}}</h2>
+    <div class="grid grid-cols-2 gap-x-4">
+        <div class="flex flex-col justify-end h-full">
             <h3 class="">Clams: {{$clam_count}}</h3>
             <button wire:click="clam_increment" class="bg-red-500 text-white py-4 px-2" >Clam Slam</button>
         </div>
-        <div class="flex flex-col content-end h-12">
+        <div class="flex flex-col justify-end h-full">
             <h3 class="">Cherish Points: {{$cp_count}}</h3>
             <button wire:click="cp_increment" class="bg-green-500 text-white py-4 px-2">Cherish Me</button>
         </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Clam Slammer</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -18,12 +18,15 @@
       @livewireStyles  
     </head>
     <body class="antialiased">
-        <div class="grid grid-cols-4 min-h-screen  sm:items-center py-4 sm:pt-0">
-            
-            @foreach ($bandmembers as $bandmember)
-            <livewire:counter :name="$bandmember->name" :clam_count="$bandmember->clam_count" :cp_count="$bandmember->cp_count" />
-            @endforeach
-            
+        <div class="flex flex-col min-h-screen justify-center items-center">
+            <h1 class="text-center text-3xl">Clam Slammer</h1>
+            <div class="grid xl:grid-cols-4 md:grid-cols-3 grid-cols-1 sm:pt-0 ">
+                
+                @foreach ($bandmembers as $bandmember)
+                <livewire:counter :name="$bandmember->name" :clam_count="$bandmember->clam_count" :cp_count="$bandmember->cp_count" />
+                @endforeach
+                
+            </div>
         </div>
         @livewireScripts
     </body>
